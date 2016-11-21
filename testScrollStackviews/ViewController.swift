156da964e1b90a63729a9f53b431bf7f8b0259e1
Michael Hurwicz <mhurwicz@gmail.com>
2016-11-21 12:12:30 -0800

    Scrolling version

--- testScrollStackviews/ViewController.swift
+++ testScrollStackviews/ViewController.swift
@@ -8,18 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet weak var mainScroll: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mainScroll.delegate = self
+        print("----- viewDidLoad -----")
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swiped(_:)))
+        swipeDown.direction = UISwipeGestureRecognizerDirection.Down
+        self.view.addGestureRecognizer(swipeDown)
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swiped(_:)))
+        swipeUp.direction = UISwipeGestureRecognizerDirection.Up
+        self.view.addGestureRecognizer(swipeUp)
+    }
+    
+    func swiped(gesture: UIGestureRecognizer)
+    {
+        if let swipeGesture =  gesture as? UISwipeGestureRecognizer
+        {
+            switch swipeGesture.direction
+            {
+            case UISwipeGestureRecognizerDirection.Down:
+                print("DOWN")
+                if (mainScroll.contentOffset.y >= 30) {
+                    mainScroll.contentOffset.y = mainScroll.contentOffset.y - 85
+                }
+            case UISwipeGestureRecognizerDirection.Up:
+                print("UP \(mainScroll.contentOffset.y)")
+                if (mainScroll.contentOffset.y <= 30*16 ) {
+                    mainScroll.contentOffset.y = mainScroll.contentOffset.y+85
+                }
+            default:
+                break
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
